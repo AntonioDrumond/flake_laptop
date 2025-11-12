b0b6600b0f81eaa5a68652bf5265149ae6bd034a
Antonio Drumond <adcdstom@gmail.com>
2025-11-12 07:34:44 -0300

    update hyprland settings

--- dot_config/waybar/autohide.sh
+++ dot_config/waybar/autohide.sh
@@ -1,30 +1,40 @@
 #!/bin/sh
 
-threshold=25
+pos_threshold=60
+vel_threshold=175
 status=0
+open=0
 
 while true; do
 
-	ypos=$(hyprctl cursorpos | cut -d' ' -f2)
 	window=$(hyprctl activeworkspace | grep windows: | cut -d' ' -f2)
+	new_ypos=$(hyprctl cursorpos | cut -d' ' -f2)
+	vel=$((ypos - new_ypos))
+	# echo "$vel - $vel_threshold"
+	# echo "$open"
+
+	if [ "$vel" -ge "$vel_threshold" ]; then
+		open=1
+	fi
 
 	if [ $window == 0 ]; then # Open when no windows are open
 
 		if [ $status != 1 ]; then
-			pkill -SIGUSR2 waybar
+			pkill -SIGUSR1 waybar
 			status=1
 		fi
-	elif [ "$ypos" -le "$threshold" ]; then # Open on hover
+	elif [ "$open" -eq 1 ] && [ "$ypos" -le "$pos_threshold" ]; then # Open on hover
 		if [ $status != 1 ]; then
-			pkill -SIGUSR2 waybar
+			pkill -SIGUSR1 waybar
 			status=1
 		fi
-	else 							   	# Close on not hover
+	elif [ "$ypos" -gt "$pos_threshold" ]; then		   	# Close on not hover
 		if [ $status != 2 ]; then
+			open=0
 			pkill -SIGUSR1 waybar
 			status=2
 		fi
 	fi
-
+	ypos=$new_ypos
 	sleep 0.1
 done
