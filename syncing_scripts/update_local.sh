6601296c84e1d8d4ca8ab124f4de4ec5af7dd8da
Antonio Drumond <adcdstom@gmail.com>
2025-11-19 09:15:46 -0300

    update syncing script

--- syncing_scripts/update_local.sh
+++ syncing_scripts/update_local.sh
@@ -5,6 +5,7 @@ cd /home/antonio/repos/flake_laptop/dot_config
 cp -r ./hypr/ /home/antonio/.config/
 cp -r ./kitty/ /home/antonio/.config/
 cp -r ./waybar/ /home/antonio/.config/
+cp -r ./fish/ /home/antonio/.config/
 
 # ~
 cd /home/antonio/repos/flake_laptop/dots
