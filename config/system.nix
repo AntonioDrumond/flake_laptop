e4abaa3f3dad327b7378fb88190d9dc78feb2afc
Antonio Drumond <adcdstom@gmail.com>
2026-04-27 15:38:29 -0300

    fix tlp

--- config/system.nix
+++ config/system.nix
@@ -24,14 +24,14 @@
         tlp = {
             enable = true;
             settings = {
-                CPU_SCALING_GOVERNOR_ON_AC = "performance";
-                CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+                # CPU_SCALING_GOVERNOR_ON_AC = "performance";
+                # CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
 
-                CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
-                CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+                # CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+                # CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
 
                 TLP_DEFAULT_MODE = "BAT";
-                TLP_PERSISTENT_DEFAULT = 1;
+                # TLP_PERSISTENT_DEFAULT = 1;
 
                 # CPU_MIN_PERF_ON_AC = 0;
                 # CPU_MAX_PERF_ON_AC = 100;
